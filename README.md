4801f47f5b0fe4ab11421812c355cdce1583ea9f
knighthood  <2109695291@qq.com>
2024-08-07 22:35:09 +0800

    更新

---
+++ README.md
@@ -0,0 +1,97 @@
+# 前言
+由于我不太会pr，所以直接新建的项目，
+
+原项目地址：https://github.com/Optimistism/Style-transfer
+
+原项目代码的讲解地址：https://www.bilibili.com/video/BV1yY4y1c7Cz/
+
+本项目是对原想木的一点点完善。
+
+# 项目如何运行
+1. 下载项目
+这个比较简单，直接在github上下载zip包，然后解压即可。或者使用git clone命令克隆项目。
+2. 安装依赖
+- 这里由于我没有用到虚拟环境，无法准确得出需要用的包，大致就是torch和我requirements.txt中的包。
+- 安装命令
+安装torch的话，直接去网上搜一下，换个源，下载更快
+```shell
+pip install -r requirements.txt
+```
+
+- 这里需要注意，需要安装tensorboard，作者给出的并没有这个，但是如果没有这个，你是查看不了运行结果的。
+3. 运行项目
+- 首先就是运行代码
+- 然后的话，它会去https://download.pytorch.org/models/vgg19-dcbb9e9d.pth   去下载vgg19的预训练权重，这个文件大概548mb。
+由于我这里不知道pytorch如何加载这个文件，因此，**大家可以自己下载这个文件，或者将我这个文件，放到你代码运行后，它给出的那个路径下**。可以看下面我更改部分第一点的第二张图片，运行代码会给出最终下载的路径的。
+- 然后想要看到tensorboard中的数据，就在终端输入
+```shell
+tensorboard --logdir=runs
+```
+然后点击浏览器，输入localhost:6006
+[点击这里也可以](http://localhost:6006/)
+
+![img.png](./讲解图/img.png)
+
+后续你跑自己的图片，只需要在`main.py`中修改`content_img`和`style_img`即可。
+# 更改
+1. 更新了vgg19传参
+最开始会报这个警告
+![1](./讲解图/vgg19更新.png)
+使用这行代码后
+```shell
+# self.vgg = models.vgg19(pretrained=True).features  # .features用于提取卷积层
+self.vgg = models.vgg19(weights=VGG19_Weights.IMAGENET1K_V1).features
+```
+这行代码，会去自动下载官网的vgg19预训练权重，548mb，下载还是很慢的。我下载这个花了30分钟。
+
+我这里由于文件太大，无法上传到github，因此，大家可以自己下载这个文件。
+
+不会警告了
+![1](./讲解图/Snipaste_2024-08-04_08-01-13.png)
+
+2. 添加了content_weight参数，更加符合其公式要求
+```shell
+loss = content_weight * content_loss + style_weight * style_loss
+```
+
+3. 运行代码后提示报错
+```shell
+RuntimeError: Input type (torch.FloatTensor) and weight type
+ (torch.cuda.FloatTensor) should be the same or input should 
+ be a MKLDNN tensor and weight is a dense tensor
+```
+当模型的权重（weights）和输入数据（inputs）不在同一个设备上时。
+
+这个命令报错表示你的模型权重被移动到了CUDA设备上（即GPU），但是你的输入数据还在CPU上。
+
+因此，具体做法是，将输入数据也移动到device中，它将根据你代码中的变换，自动选择设备。
+```shell
+# 图片输入到gpu，否则就会报错
+content_img = content_img.to(device)
+style_img = style_img.to(device)
+```
+4. **添加了日志记录**
+
+在`main.py`的最开始，添加了一些代码，主要就是设置了日志，并且以时间命名，方便后续将结果保存到日志中，方便查看。免得你自己去创建txt进行cv。
+
+![img.png](./讲解图/img6.png)
+
+可以看到，这里也会有时间，你可以通过看日志，计算训练的时间。
+# 实际体验
+
+我本机是1650的显卡，跑3000轮，花了20分钟。
+
+原图
+
+<div style="text-align:center">
+    <img src="./images/content_img.jpg" alt="1" width="200" height="200">
+</div>
+结果图
+
+梵高风的狗头自拍图
+<div style="text-align:center">
+    <img src="./result/step2999.png" alt="1" width="200" height="200">
+</div>
+
+效果其实还不错
+
---
+++ main.py
@@ -0,0 +1,149 @@
+import torch
+from PIL import Image
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms, models
+from torchvision.models.vgg import VGG19_Weights
+import logging
+from datetime import datetime
+
+import copy
+
+current_time = datetime.now().strftime('%Y-%m-%d %H.%M.%S')
+# 配置日志
+logging.basicConfig(filename='logs/style_transfer_{}.log'.format(current_time), filemode="w", level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+# 创建一个StreamHandler实例，名为console。StreamHandler是一个日志处理器，用于将日志输出到类似文件或系统控制台的流中。
+# 在这个例子中，它被配置为将日志输出到控制台（即标准输出）。
+console = logging.StreamHandler()
+
+# 设置console日志处理器的日志级别为INFO。这意味着只有INFO级别及以上（WARNING, ERROR, CRITICAL）的日志才会被这个处理器处理。
+console.setLevel(logging.INFO)
+
+# 创建一个Formatter实例，用于定义日志的格式。这里定义的格式是：时间戳 - 日志级别 - 日志消息。
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')  # %(asctime)s 表示时间戳，%(levelname)s 表示日志级别，%(message)s 表示日志消息。
+
+# 将上面定义的格式器formatter应用到console日志处理器上。
+# 这样，所有通过console处理器处理的日志都将按照指定的格式输出。
+console.setFormatter(formatter)
+
+# 获取根日志记录器（即默认的日志记录器），并给它添加一个处理器（在这个例子中是console）。
+# 这意味着所有通过根日志记录器（或未明确指定日志记录器的日志）都将被发送到console处理器，并按照设置的格式输出到控制台。
+# 注意：这里的''（空字符串）是获取根日志记录器的标准方式。
+logging.getLogger('').addHandler(console)
+
+# 设置图片大小
+img_size = 512
+
+# 设置训练设备
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Normalize(mean=[0.500, 0.500, 0.500],
+                                 std=[0.229, 0.224, 0.225])
+# 加载图片
+def load_img(img_path):
+    # 使打开的图片通道为RGB格式,如果不使用.convert('RGB')进行转换的话，读出来的图像是RGBA四通道的，A通道为透明通道，该对深度学习模型训练来说暂时用不到，因此使用convert('RGB')进行通道转换。
+    img = Image.open(img_path).convert('RGB')
+    img = img.resize((img_size, img_size))  # 对图片进行裁剪，为512x512
+    img = transforms.ToTensor()(img)
+    img = transform(img).unsqueeze(0)  # unsqueeze升维，使数据格式符合[batch_size, n_channels, hight, width],[1,3,512,512]
+    return img
+
+# 显示图片
+def show_img(tensor, save_path=None):
+    image = tensor.cpu().clone()
+    image = image.squeeze(0)  # 图片第一维为batch_size，将其降维
+    if save_path is not None:
+        transform = transforms.ToPILImage()
+        image_pil = transform(image)
+        image_pil.save(save_path)
+    return image
+
+# 构建神经网络
+class VGGNet(nn.Module):
+    def __init__(self):
+        super(VGGNet, self).__init__()
+        self.select = ['0', '5', '10', '19', '28']
+        # self.vgg = models.vgg19(pretrained=True).features  # .features用于提取卷积层
+        self.vgg = models.vgg19(weights=VGG19_Weights.IMAGENET1K_V1).features
+
+    def forward(self, x):
+        features = []
+        for name, layer in self.vgg._modules.items():
+            x = layer(x)  # name为第几层的序列号，layer就是卷积层,,x为输入的图片。x = layer(x)的意思是，x经过layer层卷积后再赋值给x
+            if name in self.select:
+                features.append(x)
+
+        return features
+
+# for name, layer in models.vgg19(weights=VGG19_Weights.IMAGENET1K_V1).features._modules.items():
+#     print(name)
+#     print(layer)
+
+# 加载图片
+content_img = load_img("images/content_img.jpg")
+style_img = load_img("images/赛博风格.png")
+
+# 图片输入到gpu，否则就会报错
+content_img = content_img.to(device)
+style_img = style_img.to(device)
+
+target = content_img.clone().requires_grad_(True)  # clone()操作后的tensor requires_grad=True，clone操作在不共享数据内存的同时支持梯度梯度传递与叠加
+optimizer = torch.optim.Adam([target], lr=0.003)  # 选择优化器
+vgg = VGGNet().to(device).eval()
+total_step = 3000  # 训练次数
+
+content_weight = 1  # 给content_loss加上的权重
+style_weight = 100  # 给style_loss加上的权重
+
+# 设置tensorboard，用于可视化
+writer = SummaryWriter("runs")
+
+"""detach() 函数的作用是返回一个与当前计算图（graph）分离的新张量（tensor），
+该张量不再需要计算梯度。这意呀着，当你对返回的张量进行任何操作时，这些操作不会影响到原始张量所在的计算图，
+也不会被PyTorch的自动求导系统（autograd）所追踪。"""
+content_features = [x.detach() for x in vgg(content_img)]
+style_features = [x.detach() for x in vgg(style_img)]
+
+# 开始训练
+for step in range(1, total_step+1):
+    target_features = vgg(target)
+
+    style_loss = 0
+    content_loss = 0
+    for f1, f2, f3 in zip(target_features, content_features, style_features):
+        content_loss = torch.mean((f1 - f2) ** 2) + content_loss
+        _, c, h, w = f1.size()  # 结果为torch.Size([1, 64, 512, 512])
+        f1 = f1.view(c, h * w)  # 处理数据格式为后面gram计算
+        f3 = f3.view(c, h * w)
+
+        # 计算gram matrix
+        f1 = torch.mm(f1, f1.t())  # torch.mm()两个矩阵相乘,.t()是矩阵倒置
+        f3 = torch.mm(f3, f3.t())
+        style_loss = torch.mean((f1 - f3) ** 2) / (c * h * w) + style_loss
+
+    loss = content_weight * content_loss + style_weight * style_loss
+
+    # 更新target
+    optimizer.zero_grad()  # 每一次优化都要梯度清零
+    loss.backward()  # 反向传播
+    optimizer.step()
+    writer.add_scalar("loss", loss, step)
+    denorm = transforms.Normalize((-2.12, -2.04, -1.80), (4.37, 4.46, 4.44))
+    img = target.clone().squeeze()  # 升维-->降维
+    img = denorm(img).clamp_(0, 1)
+    if step % 500 == 0:   # 每500轮保存图片
+        img = show_img(img, "result/step{}.png".format(step))
+    else:
+        img = show_img(img)
+    writer.add_image("target", img, global_step=step)
+    # print("Step [{}/{}], Content Loss: {:.4f}, Style Loss: {:.4f}"
+    #       .format(step, total_step, content_loss.item(), style_loss.item()))
+    logging.info("Step [{}/{}], Content Loss: {:.4f}, Style Loss: {:.4f}, loss: {:.2f}"
+                 .format(step, total_step, content_loss.item(), style_loss.item(), loss.item()))
+
+    if step % 500 == 0:
+        current_model = copy.deepcopy(vgg.state_dict())
+        torch.save(current_model, "save_model/model_{}.pth".format(step))
+writer.close()
